c65799c08089ae3d18b177763d08d626874dbba1
yobe8 <sanyobe@gmail.com>
2021-05-10 13:57:12 +0900

    Add sign in admin feature

--- routes/admin.js
+++ routes/admin.js
@@ -1,8 +1,9 @@
 const router = require("express").Router();
 
-const { authAdmin, signUpAdmin } = require("./controllers/admin.controller");
+const { authAdmin, signUpAdmin, signInAdmin } = require("./controllers/admin.controller");
 
 router.post("/authentication", authAdmin);
 router.post("/sign-up", signUpAdmin);
+router.post("/sign-in", signInAdmin);
 
 module.exports = router;
