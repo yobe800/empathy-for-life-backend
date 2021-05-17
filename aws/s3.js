b0fc3ceaa51e8f5e19b4a5ac3faa776189ef8bcb
yobe8 <sanyobe@gmail.com>
2021-05-17 19:45:45 +0900

    Add CRUD features for posts

--- aws/s3.js
+++ aws/s3.js
@@ -1,22 +1,23 @@
-const AWS = require('aws-sdk');
+const AWS = require("aws-sdk");
 
 const { AWS_ACCESS_KEY_ID, AWS_SECRET_KEY, AWS_REGION, AWS_S3_BUCKET } = process.env;
 
-AWS.config.setPromisesDependency(require('bluebird'));
+AWS.config.setPromisesDependency(require("bluebird"));
 AWS.config.update({ accessKeyId: AWS_ACCESS_KEY_ID, secretAccessKey: AWS_SECRET_KEY, region: AWS_REGION });
 
 const s3 = new AWS.S3();
 
 const uploadPhotoToS3 = async (fileKey = "", route = "", base64) => {
-  const base64Data = new Buffer.from(base64.replace(/^data:image\/\w+;base64,/, ""), 'base64');
-  const type = base64.split(';')[0].split('/')[1];
+  const base64Data = new Buffer.from(base64.replace(/^data:image\/\w+;base64,/, ""), "base64");
+  console.log(base64Data);
+  const type = base64.split(";")[0].split("/")[1];
 
   const params = {
     Bucket: `${AWS_S3_BUCKET}/${route}`,
     Key: `${fileKey}${Date.now()}.${type}`,
     Body: base64Data,
-    ACL: 'public-read',
-    ContentEncoding: 'base64',
+    ACL: "public-read",
+    ContentEncoding: "base64",
     ContentType: `image/${type}`,
   };
 
--- routes/posts.js
+++ routes/posts.js
@@ -2,8 +2,16 @@ const router = require("express").Router();
 
 const {
   addPost,
+  getPosts,
+  getPost,
+  editPost,
+  deletePost,
 } = require("./controllers/posts.controller");
 
 router.post("/new", addPost);
+router.get("/", getPosts);
+router.get("/:id", getPost);
+router.put("/:id", editPost);
+router.delete("/:id", deletePost);
 
 module.exports = router;
