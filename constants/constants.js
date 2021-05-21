58e70c8be1615f4eb12ff47baa24d082acd7ea61
yobe8 <sanyobe@gmail.com>
2021-05-22 02:05:32 +0900

    Correct update all dogs time for presentation

--- constants/constants.js
+++ constants/constants.js
@@ -8,7 +8,7 @@ const SCHEMA_TIMESTAMPS_OPTION = {
 const USER_CHARACTERS = Array(16).fill(null).map((_, index) => `person${index}`);
 
 const ID_TOKEN_COOKIE_MAX_AGE = 24 * 60 * 60 * 1000;
-const UPDATE_DOGS_TIME = 15 * 60 * 1000;
+const UPDATE_DOGS_TIME = 3 * 60 * 1000;
 
 const CANVAS_SIZES = {
   x: 1000,
